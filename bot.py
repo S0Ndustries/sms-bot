dab47c31e7b4563efc7aac5dd471e435862ccf25
Josh Carnide <joshuacarnide@gmail.com>
2015-08-19 00:18:54 -0400

    added DB boilerplate

--- bot.py
+++ bot.py
@@ -2,8 +2,12 @@ from flask import Flask, request, Response
 from verification import verify_signature
 import requests
 import os
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = postgres://lcgigjguzxyusa:Fl-W3fEmvP5mmgRSgoYiEF63Ro@ec2-54-197-230-210.compute-1.amazonaws.com:5432/dekjsm7qjhos9c
+db = SQLAlchemy(app)
 
 
 @app.route('/receive', methods=['POST'])
@@ -18,14 +22,24 @@ def receive_messages():
     responses = []
 
     for message in messages:
-        # create a response to each received message just echoing the body text
-        if message['type'] == 'text':
+        #Greeting message on initial scan
+        if message['type'] == 'scan-data':
             responses.append({
                 'type': 'text',
                 'to': message['from'],
-                'body': 'You said "{}"'.format(message['body'])
+                'body': 'Welcome to SMS Bot. I can help you send an SMS message to any number in the United States or Canada. What would you like to do?',
+                'suggestedResponses': ['Send a New Message', 'Help']
+            })
+        #Other responses    
+        elif message['type'] == 'text':
+            if message['body'] == 'Send a New Message':
+                responses.append({
+                'type': 'text',
+                'to': message['from'],
+                'body': 'Please enter a US or Canadian cellphone number' 
             })
 
+           
     if responses:
         # send the responses through the Chat Engine API
         requests.post(
